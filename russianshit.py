dd85c3c3fc35fe7d002839e87d3a240e91667058
awesomej126 <68038306+awesomej126@users.noreply.github.com>
2022-02-21 15:46:04 -0500

    Delete russianshit.py

--- russianshit.py
+++
@@ -1,9 +0,0 @@
-# Russian Reversal Joke Generator
-# Idea by Mary Zotter
-
-# Words
-noun = input("Enter a noun: ")
-verb = input("Enter a verb: ")
-
-print("In America, you " + verb + " " + noun + ",")
-print("In Soviet Russia, " + noun + " " + verb + " you.")
