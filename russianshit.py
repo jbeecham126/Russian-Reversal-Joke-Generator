469a470f6f9d4516781ae9795223aa551e6b187c
Jules <68038306+awesomej126@users.noreply.github.com>
2022-02-21 15:43:56 -0500

    Add files via upload

---
+++ russianshit.py
@@ -0,0 +1,9 @@
+# Russian Reversal Joke Generator
+# Idea by Mary Zotter
+
+# Words
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print("In America, you " + verb + " " + noun + ",")
+print("In Soviet Russia, " + noun + " " + verb + " you.")
